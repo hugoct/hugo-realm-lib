9275c4b959556e24bc578254dcd6051291e6203e
Hugo <hugo.ct93@gmail.com>
2019-07-28 10:56:58 -0300

    .

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
 import { NativeModules } from "react-native";
 
-export const insert = (obj, callback) => {
+const insert = (obj, callback) => {
   NativeModules.libRealm.insert(JSON.stringify(obj), (res, error) =>
     callback(JSON.parse(res), JSON.parse(error))
   );
 };
 
-export const insertMany = (objs, callback) => {
+const insertMany = (objs, callback) => {
   NativeModules.libRealm.insertMany(JSON.stringify(objs), (res, error) =>
     callback(JSON.parse(res), JSON.parse(error))
   );
 };
 
-export const insertOrUpdateMany = (objs, callback) => {
+const insertOrUpdateMany = (objs, callback) => {
   NativeModules.libRealm.insertOrUpdateMany(
     JSON.stringify(objs),
     (res, error) => callback(JSON.parse(res), JSON.parse(error))
   );
 };
 
-export const update = (obj, callback) => {
+const update = (obj, callback) => {
   NativeModules.libRealm.update(JSON.stringify(obj), (res, error) =>
     callback(JSON.parse(res), JSON.parse(error))
   );
 };
 
-export const deleteOne = (id, callback) => {
+const deleteOne = (id, callback) => {
   NativeModules.libRealm.delete(id, (res, error) =>
     callback(JSON.parse(res), JSON.parse(error))
   );
 };
 
-export const deleteMany = (ids, callback) => {
+const deleteMany = (ids, callback) => {
   NativeModules.libRealm.deleteMany(JSON.stringify(ids), (res, error) =>
     callback(JSON.parse(res), JSON.parse(error))
   );
 };
 
-export const select = callback => {
+const select = callback => {
   NativeModules.libRealm.select((res, error) =>
     callback(JSON.parse(res), JSON.parse(error))
   );
+};
+
+export default {
+  select,
+  insert,
+  insertMany,
+  update,
+  insertOrUpdateMany,
+  deleteOne,
+  deleteMany
 };
